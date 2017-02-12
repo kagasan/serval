c72def549a656d2e3c8fc761c03a088a8f38e5c6
kagasan <ghghkaja@gmail.com>
2017-02-12 16:25:07 +0900

    hoge

--- docs/script.js
+++ docs/script.js
@@ -6,11 +6,18 @@ onload = function(){
 }
 
 function make(){
+	var form = document.forms.fm;
+	var sz = parseInt(form.sz.value);
+	var ta = form.ta.value;
 	var canvas = document.createElement("canvas");
 	canvas.width = 800;
 	canvas.height = 450;
 	var ctx = canvas.getContext('2d');
 	ctx.drawImage(img, 0, 0);
+	ctx.font = ""+sz+"px"+" 'メイリオ'";
+	ctx.fillStyle = "rgb(0,0,0)";
+	var txts = ta.split("\n");
+	for(var i=0;i<txts.length;i++)ctx.fillText(txts[i],31,225+sz+sz*i-sz*txts.length/2);
 	document.getElementById("output").innerHTML = "<img src='" + canvas.toDataURL() + "'></br>";
 	
 }
